8eb9d43d8a8fb1fc0a639ba375a2038fd5ccf0f9
Ryo Vamirion <rynortheast@yandex.ru>
2022-09-29 16:33:06 +0300

    Исправлена функция s21_mult_matrix - неправильное умножение

--- src/core/s21_arithmetics.c
+++ src/core/s21_arithmetics.c
@@ -9,7 +9,20 @@ int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
 }
 
 int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
-  return simple_arithmetics(A, B, result, 3);
+  int status = (s21_is_bad_matrix(A) || s21_is_bad_matrix(B));
+
+  if (status == 0 && (status = A->columns != B->rows ? 2 : 0) == 0) {
+    if ((status = s21_create_matrix(A->rows, A->columns, result)) == 0) {
+      for (int x = 0; x < A->rows; x += 1) {
+        for (int y = 0; y < B->columns; y += 1) {
+          for (int z = 0; z < B->rows; z += 1)
+            result->matrix[x][y] += A->matrix[x][z] * B->matrix[z][y];
+        }
+      }
+    }
+  }
+
+  return status;
 }
 
 int s21_mult_number(matrix_t *A, double number, matrix_t *result) {
@@ -18,9 +31,8 @@ int s21_mult_number(matrix_t *A, double number, matrix_t *result) {
   if (status == 0) {
     if ((status = s21_create_matrix(A->rows, A->columns, result)) == 0) {
       for (int x = 0; x < A->rows; x += 1) {
-        for (int y = 0; y < A->columns; y += 1) {
+        for (int y = 0; y < A->columns; y += 1)
           result->matrix[x][y] = A->matrix[x][y] * number;
-        }
       }
     }
   }
@@ -39,8 +51,6 @@ int simple_arithmetics(matrix_t *A, matrix_t *B, matrix_t *result, int znak) {
             result->matrix[x][y] = A->matrix[x][y] + B->matrix[x][y];
           else if (znak == 2)
             result->matrix[x][y] = A->matrix[x][y] - B->matrix[x][y];
-          else if (znak == 3)
-            result->matrix[x][y] = A->matrix[x][y] * B->matrix[x][y];
         }
       }
     }
