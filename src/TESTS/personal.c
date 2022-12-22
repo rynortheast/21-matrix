6441be2a3df87832d92f304c8bbf39b1889288fc
Ryo Vamirion <rynortheast@yandex.ru>
2022-12-22 22:49:07 +0300

    Обновлен персональный тест как пример

--- src/TESTS/personal.c
+++ src/TESTS/personal.c
@@ -1,19 +1,54 @@
 #include "../s21_matrix.h"
 
 int main() {
-  int res;
   matrix_t A = {0};
   matrix_t Z = {0};
+  matrix_t X = {0};
 
-  s21_create_matrix(1, 1, &A);
-  A.matrix[0][0] = 2;
+  s21_create_matrix(4, 4, &A);
+  A.matrix[0][0] = -1.0;
+  A.matrix[0][1] = 2.0;
+  A.matrix[0][2] = 7.0;
+  A.matrix[0][3] = 9.0;
+  A.matrix[1][0] = 1.0;
+  A.matrix[1][1] = 0.0;
+  A.matrix[1][2] = 0.0;
+  A.matrix[1][3] = 0.0;
+  A.matrix[2][0] = 47.0;
+  A.matrix[2][1] = 13.0;
+  A.matrix[2][2] = 17.0;
+  A.matrix[2][3] = 21.0;
+  A.matrix[3][0] = 22.0;
+  A.matrix[3][1] = 7.0;
+  A.matrix[3][2] = 1.0;
+  A.matrix[3][3] = 3.0;
 
-  res = s21_inverse_matrix(&A, &Z);
+  s21_inverse_matrix(&A, &Z);
+
+  s21_create_matrix(4, 4, &X);
+
+  X.matrix[0][0] = 0.0;
+  X.matrix[0][1] = 1.0;
+  X.matrix[0][2] = 0.0;
+  X.matrix[0][3] = 0.0;
+  X.matrix[1][0] = -5.0 / 23.0;
+  X.matrix[1][1] = -121.0 / 23.0;
+  X.matrix[1][2] = 2.0 / 23.0;
+  X.matrix[1][3] = 1.0 / 23.0;
+  X.matrix[2][0] = -18.0 / 23.0;
+  X.matrix[2][1] = -379.0 / 46.0;
+  X.matrix[2][2] = 19.0 / 46.0;
+  X.matrix[2][3] = -25.0 / 46.0;
+  X.matrix[3][0] = 53.0 / 69.0;
+  X.matrix[3][1] = 1061.0 / 138.0;
+  X.matrix[3][2] = -47.0 / 138.0;
+  X.matrix[3][3] = 19.0 / 46.0;
+
+  s21_eq_matrix(&X, &Z);
 
   s21_remove_matrix(&A);
+  s21_remove_matrix(&X);
   s21_remove_matrix(&Z);
-
-  printf("RESULT = %d\n", res);
 
   return 0;
 }
